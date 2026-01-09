6762904be46de8466aa60f5ced5c7b44e1cc0744
Drive 1/Theodore <benji.urbancic@gmail.com>
2026-01-08 18:00:04 -0800

    Indexed?

--- src/main/java/org/sert2521/marvin2026/Constants.kt
+++ src/main/java/org/sert2521/marvin2026/Constants.kt
@@ -7,6 +7,8 @@ import yams.gearing.MechanismGearing
 object ElectronicIDs {
     const val FLYWHEEL_MOTOR_TOP_ID = 1
     const val FLYWHEEL_MOTOR_BOTTOM_ID = 2
+    const val INDEXER_MOTOR_ID = 3
+    const val INDEXER_BEAMBREAK_ID = 4
 }
 
 object FlywheelsConstants {
@@ -24,4 +26,18 @@ object FlywheelsConstants {
     )
 
     val ShootTarget = RPM.of(0.0)
+}
+
+object IndexerConstants {
+    val indexerGearing = MechanismGearing(
+        GearBox.fromReductionStages(
+            6.75
+        )
+    )
+
+    const val DEFAULT = 0.0
+
+    const val INDEXING = 0.3
+
+    const val REVERSE = -0.2
 }
--- src/main/java/org/sert2521/marvin2026/subsystems/indexer/IndexerSubsystem.kt
+++ src/main/java/org/sert2521/marvin2026/subsystems/indexer/IndexerSubsystem.kt
@@ -1,4 +1,73 @@
 package org.sert2521.marvin2026.subsystems.indexer
 
-object IndexerSubsystem {
+import com.revrobotics.spark.SparkLowLevel
+import com.revrobotics.spark.SparkMax
+import edu.wpi.first.math.system.plant.DCMotor
+import edu.wpi.first.networktables.NetworkTableInstance
+import edu.wpi.first.units.Units.Amps
+import edu.wpi.first.wpilibj.DigitalInput
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.sert2521.marvin2026.ElectronicIDs
+import org.sert2521.marvin2026.ElectronicIDs.INDEXER_BEAMBREAK_ID
+import org.sert2521.marvin2026.IndexerConstants
+import yams.motorcontrollers.SmartMotorControllerConfig
+import yams.motorcontrollers.local.SparkWrapper
+import yams.telemetry.MechanismTelemetry
+
+object IndexerSubsystem : SubsystemBase() {
+    private val indexerMotor = SparkMax(ElectronicIDs.INDEXER_MOTOR_ID, SparkLowLevel.MotorType.kBrushless)
+    private val indexerBeambreak = DigitalInput(INDEXER_BEAMBREAK_ID)
+
+    private val indexerMotorConfig = SmartMotorControllerConfig(this)
+        .withGearing(IndexerConstants.indexerGearing)
+        .withIdleMode(SmartMotorControllerConfig.MotorMode.BRAKE)
+        .withTelemetry("Indexer Motor", SmartMotorControllerConfig.TelemetryVerbosity.HIGH)
+        .withStatorCurrentLimit(Amps.of(40.0))
+        .withMotorInverted(false)
+        .withControlMode(SmartMotorControllerConfig.ControlMode.OPEN_LOOP)
+
+    private val indexerSMC = SparkWrapper(indexerMotor, DCMotor.getNEO(1), indexerMotorConfig)
+
+    private val telemetry = MechanismTelemetry()
+
+    init {
+        defaultCommand = default()
+
+        indexerSMC.setupTelemetry(
+            NetworkTableInstance.getDefault().getTable("Tuning")
+                .getSubTable("Indexer"),
+            NetworkTableInstance.getDefault().getTable("Mechanisms")
+                .getSubTable("Indexer")
+        )
+    }
+
+    override fun periodic() {
+        indexerSMC.updateTelemetry()
+    }
+
+    override fun simulationPeriodic() {
+        indexerSMC.simIterate()
+    }
+
+    private fun setIndexerMotor(dutyCycle: Double) {
+        indexerSMC.dutyCycle = dutyCycle
+    }
+
+    fun index(): Command {
+        return runOnce {
+            setIndexerMotor(IndexerConstants.INDEXING)
+        }.until(
+            indexerBeambreak.get == true
+        )
+    }
+
+    fun reverse():Command {
+        return runOnce {
+            setIndexerMotor(IndexerConstants.REVERSE)
+        }.andThen(
+            Commands.idle()
+        )
+    }
 }
